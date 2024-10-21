11df1a5198831fe7954ef8c251b7408b0013d611
Marcella Ivina <152522132+MarcyIvi@users.noreply.github.com>
2024-10-21 16:53:43 -0300

    Add files via upload

---
+++ OperadoresAritimeticos/src/br/edu/principal/Principal.java
@@ -0,0 +1,48 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		// Alguns números
+		int i = 27;
+		int j = 42;
+		double x = 27.475;
+		double y = 7.22;
+		
+		System.out.println("Valor das variáveis...");
+		System.out.println("i = " + i);
+		System.out.println("j = " + j);
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		
+		// Adição dos Números
+		
+		System.out.println("i + j = " + (i + j));
+		System.out.println("x + y = " + (x + y));
+		System.out.println("i + x = " + (i + x));
+		System.out.println("j + y = " + (j + x));
+		
+		// Subtração dos Números
+		
+		System.out.println("i - j = " + (i - j));
+		System.out.println("x - y = " + (x - y));
+		System.out.println("i - x = " + (i - x));
+		System.out.println("j - y = " + (j - y));
+		
+		// Mulplicação dos Números
+
+		System.out.println("i * j = " + (i * j));
+		System.out.println("x * y = " + (x * y));
+		System.out.println("i * x = " + (i * x));
+		System.out.println("j * y = " + (j * y));
+		
+		// Subtração dos Números
+		
+		System.out.println("i / j = " + (i / j));
+		System.out.println("x / y = " + (x / y));
+		System.out.println("i / x = " + (i / x));
+		System.out.println("j / y = " + (j / y));
+	}
+
+}
